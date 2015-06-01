19475ad93e8142e6b25b0a559614d620b7b4813c
Alessandro Muiesan <ale@ctu.unimi.it>
2015-06-01 18:12:58 +0200

    utilizzo del currentyear in servizio

--- app/scripts/services/unimiservice.js
+++ app/scripts/services/unimiservice.js
@@ -10,4 +10,8 @@
 angular.module('unimiAppApp')
   .service('unimiService', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
+	this.restUrl = 'http://192.168.15.13/unimiRest/unimi/';
+	this.currentYear = '2015';
+	
+	
   });
