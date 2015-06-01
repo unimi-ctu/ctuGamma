bdbcd43a255bf2a174fd19de50145c95d4705abb
Alessandro Muiesan <amuiesan@hotmail.com>
2015-06-01 13:08:28 +0200

    completa la ricerca

--- app/scripts/controllers/quicksearch.js
+++ app/scripts/controllers/quicksearch.js
@@ -8,12 +8,19 @@
  * Controller of the unimiAppApp
  */
 angular.module('unimiAppApp')
-  .controller('QuicksearchCtrl', function ($scope, $http, $routeParams) {
+  .controller('QuicksearchCtrl', function ($scope, $http, $routeParams, $rootScope) {
+	  $scope.limits = {}
+	  
 	  $scope.search = function(keywords) {
 		$http.get('http://192.168.15.13/unimiRest/unimi/quicksearch/' + keywords).success(function(data) {
+			$scope.limits = { pmax: 10, tmax: 10, cdsmax: 10, afmax: 10 };
+			
 			$scope.results = data;
-		})
+		});
 	  };
 	  
-	  $scope.search($routeParams.keywords)
+	  if ($routeParams.keywords) {
+		  $rootScope.keywords = $routeParams.keywords;
+		$scope.search($routeParams.keywords);
+	  }
   });
