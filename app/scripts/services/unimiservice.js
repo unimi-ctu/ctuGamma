d89c71af0753314a21aa31553fef6cdcc34558e3
Alessandro Muiesan <amuiesan@hotmail.com>
2015-06-01 16:41:02 +0200

    aggiunge service

---
+++ app/scripts/services/unimiservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name unimiAppApp.unimiService
+ * @description
+ * # unimiService
+ * Service in the unimiAppApp.
+ */
+angular.module('unimiAppApp')
+  .service('unimiService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
