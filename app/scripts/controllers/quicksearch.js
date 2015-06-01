383f01a065174794994f4f4fac1df9bf5d28347e
Alessandro Muiesan <ale@ctu.unimi.it>
2015-06-01 17:37:25 +0200

    migliramenti motore ricerca

--- app/scripts/controllers/quicksearch.js
+++ app/scripts/controllers/quicksearch.js
@@ -14,8 +14,13 @@ angular.module('unimiAppApp')
 	  $scope.search = function(keywords) {
 		$http.get('http://192.168.15.13/unimiRest/unimi/quicksearch/' + keywords).success(function(data) {
 			$scope.limits = { pmax: 10, tmax: 10, cdsmax: 10, afmax: 10 };
-			
 			$scope.results = data;
+			
+			var listCount = (data.ProjectList.length ? 1 : 0) + (data.TeacherList.length ? 1 : 0) + (data.CdsList.length ? 1 : 0) + (data.AFList.length ? 1 : 0);
+			
+			if (listCount > 1) {
+				$scope.isPClosed = $scope.isTClosed = $scope.isCdsClosed = $scope.isAFClosed = true;
+			}
 		});
 	  };
 	  
